968f53679ee70f8d3fbe765b774be7e1de78f28b
naonao96 <world.ecolife@gmail.com>
2026-05-17 12:56:59 +0900

    Implement stincky note design.

--- run.py
+++ run.py
@@ -15,6 +15,6 @@ with app.app_context():
         print("❌Database connection failed:", e)
 
 if __name__ == '__main__':     
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
     
