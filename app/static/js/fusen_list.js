86d15c7eec1f3b64e78c363a4019bdcb71bc5991
naonao96 <world.ecolife@gmail.com>
2026-05-27 22:19:25 +0900

    Improve sticky note UI and add menu button

--- app/static/js/fusen_list.js
+++ app/static/js/fusen_list.js
@@ -1,3 +1,13 @@
 "use strict"
 
 "付箋一覧に関する処理を追記します。"
+document.addEventListener("DOMContentLoaded", () => {
+    const menuButtons = document.querySelectorAll(".fusen-menu-button");
+
+    menuButtons.forEach((button) =>{
+        button.addEventListener("click", (e) => {
+            e.stopPropagation();
+            console.log("メニュー押下！！", e);
+        })
+    })
+})
