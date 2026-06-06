5cdd194722e33f694e4ff99f354a77010701b6ff
naonao96 <world.ecolife@gmail.com>
2026-06-06 23:32:31 +0900

    Improve login page styling and add Google login button

--- app/main_routes.py
+++ app/main_routes.py
@@ -5,8 +5,8 @@ main_bp = Blueprint('main', __name__)
 
 @main_bp.route("/")
 def root():
-    debug_flg : str = Config.DEBUG_FLG
-    if debug_flg == "LOGIN_TEST_MODE":
+    #debug_flg : str = Config.DEBUG_FLG
+    #if debug_flg == "LOGIN_TEST_MODE":
         return redirect(url_for("auth.login_home"))
-    else:
-        return redirect(url_for("notes.index"))
+    #else:
+    #    return redirect(url_for("notes.index"))
